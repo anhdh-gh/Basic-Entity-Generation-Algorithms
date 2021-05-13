39f5afd7981a7a757d1892bf4e0fff91d1016067
DoHungAnh-27-09-2000 <79781308+DoHungAnh-27-09-2000@users.noreply.github.com>
2021-05-13 23:14:24 +0700

    update view

--- src/ky_thuat_do_hoa/Bai1.java
+++ src/ky_thuat_do_hoa/Bai1.java
@@ -1,7 +1,10 @@
 package ky_thuat_do_hoa;
 
-public class Bai1 extends javax.swing.JPanel {
+import java.awt.*;
+import javax.swing.*;
 
+public class Bai1 extends JPanel {
+    
     public Bai1() {
         initComponents();
     }
@@ -10,27 +13,30 @@ public class Bai1 extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         showPoint = new javax.swing.JTextArea();
-        view = new javax.swing.JPanel();
         controller = new javax.swing.JPanel();
         start = new javax.swing.JButton();
         quick = new javax.swing.JButton();
+        reset = new javax.swing.JButton();
 
         setLayout(new java.awt.BorderLayout());
 
-        jScrollPane1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Points", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Dialog", 1, 14))); // NOI18N
+        jPanel1.setLayout(new java.awt.BorderLayout());
+
+        jScrollPane1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Points", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Dialog", 1, 14))); // NOI18N
 
         showPoint.setEditable(false);
         showPoint.setColumns(20);
         showPoint.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
         showPoint.setRows(5);
+        showPoint.setOpaque(false);
         jScrollPane1.setViewportView(showPoint);
 
-        add(jScrollPane1, java.awt.BorderLayout.LINE_START);
+        jPanel1.add(jScrollPane1, java.awt.BorderLayout.LINE_START);
 
-        view.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "View", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Dialog", 1, 14))); // NOI18N
-        view.setLayout(new java.awt.BorderLayout());
+        controller.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Controller", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Dialog", 1, 14))); // NOI18N
 
         start.setText("Start");
         controller.add(start);
@@ -38,18 +44,27 @@ public class Bai1 extends javax.swing.JPanel {
         quick.setText("Quick");
         controller.add(quick);
 
-        view.add(controller, java.awt.BorderLayout.PAGE_END);
+        reset.setText("Reset");
+        controller.add(reset);
 
-        add(view, java.awt.BorderLayout.CENTER);
+        jPanel1.add(controller, java.awt.BorderLayout.PAGE_END);
+
+        add(jPanel1, java.awt.BorderLayout.LINE_START);
     }// </editor-fold>//GEN-END:initComponents
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); 
 
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel controller;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton quick;
+    private javax.swing.JButton reset;
     private javax.swing.JTextArea showPoint;
     private javax.swing.JButton start;
-    private javax.swing.JPanel view;
     // End of variables declaration//GEN-END:variables
 }
