cfc23d77cdbb570774f4c9e374f8e678338f4ca6
ToanCoi <nguyentoan110720@gmail.com>
2021-05-13 16:04:39 +0700

    add point

--- src/ky_thuat_do_hoa/MainFrame.java
+++ src/ky_thuat_do_hoa/MainFrame.java
@@ -2,13 +2,17 @@ package ky_thuat_do_hoa;
 
 public class MainFrame extends javax.swing.JFrame {
 
+    private Bai2 bai2 = new Bai2();
     public MainFrame() {
         initComponents();
+        tablePanel.add(bai2);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+
+        tablePanel = new javax.swing.JTabbedPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -16,11 +20,11 @@ public class MainFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 782, Short.MAX_VALUE)
+            .addComponent(tablePanel, javax.swing.GroupLayout.DEFAULT_SIZE, 787, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 406, Short.MAX_VALUE)
+            .addComponent(tablePanel, javax.swing.GroupLayout.DEFAULT_SIZE, 409, Short.MAX_VALUE)
         );
 
         pack();
@@ -45,5 +49,6 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTabbedPane tablePanel;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/ky_thuat_do_hoa/Point.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ky_thuat_do_hoa;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author nguye
+ */
+public class Point {
+    private int x, y, width;
+    private Color color;
+
+    public Point(int x, int y, int width, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    
+    public void draw(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g.create();
+        
+        g2d.setStroke(new BasicStroke(this.width));
+        g2d.setColor(color);
+        g2d.drawLine(x, y, x, y);
+    }
+}
