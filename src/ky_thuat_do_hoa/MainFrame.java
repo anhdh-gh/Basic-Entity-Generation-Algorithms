c5a0594f073b1dea9b9763779dd65d069daaa040
ToanCoi <nguyentoan110720@gmail.com>
2021-05-28 12:02:07 +0700

    add clock

--- src/ky_thuat_do_hoa/MainFrame.java
+++ src/ky_thuat_do_hoa/MainFrame.java
@@ -10,6 +10,7 @@ public class MainFrame extends javax.swing.JFrame {
     private final Bai4_Midpoint bai4_MidPoint = new Bai4_Midpoint();
     private final Bai9_GiaoDiem2DoanThang bai9 = new Bai9_GiaoDiem2DoanThang();
     private final Bai8_DaGiac bai8 = new Bai8_DaGiac();
+    private final Clock clock = new Clock();
     
     
     public MainFrame() {
@@ -21,7 +22,7 @@ public class MainFrame extends javax.swing.JFrame {
         view.add("Đường elip Bresemham", bai4_Bresemham); 
         view.add("Đường elip Midpoint", bai4_MidPoint); 
         view.add("Vẽ đa giác", bai8); 
-        view.add("Tìm giao điểm hai đoạn thẳng", bai9);
+        
     }
 
     @SuppressWarnings("unchecked")
